278e9bb7c581414df7fc89df67d6c4101eb95434
dnjawm19 <dnjawm19@naver.com>
2024-10-01 13:02:34 +0900

    refactor: auditing base모듈에서 가져오도록 수정

--- com.flash.flashsale/src/main/java/com/flash/flashsale/FlashsaleApplication.java
+++ com.flash.flashsale/src/main/java/com/flash/flashsale/FlashsaleApplication.java
@@ -2,10 +2,12 @@ package com.flash.flashsale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
-@EnableJpaAuditing
+@EnableFeignClients
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.flash.base", "com.flash.flashsale"})
 public class FlashsaleApplication {
     public static void main(String[] args) {
         SpringApplication.run(FlashsaleApplication.class, args);
