751bedbbf67dc755892595cea583c0cd1cbe5706
macdduck <knh9612@naver.com>
2024-10-01 20:34:38 +0900

    feat: Vendor에서 오는 FeingClient요청 응답

--- com.flash.user/src/main/java/com/flash/user/infrastructure/repository/JpaUserRepository.java
+++ com.flash.user/src/main/java/com/flash/user/infrastructure/repository/JpaUserRepository.java
@@ -4,6 +4,8 @@ import com.flash.user.domain.model.User;
 import com.flash.user.domain.repository.UserRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface JpaUserRepository extends JpaRepository<User, Long>, UserRepository {
 
     User save(User user);
@@ -11,4 +13,6 @@ public interface JpaUserRepository extends JpaRepository<User, Long>, UserReposi
     boolean existsByEmail(String email);
 
     boolean existsByPhone(String phone);
+
+    Optional<User> findById(String userId);
 }
