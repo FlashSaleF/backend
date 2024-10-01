11d0e4a7bf8c49d10ab591dc1add88890fba7e70
macdduck <knh9612@naver.com>
2024-10-01 17:09:57 +0900

    feat: 회원가입 시 필요한 base모듈 수정

--- com.flash.user/src/main/java/com/flash/user/UserApplication.java
+++ com.flash.user/src/main/java/com/flash/user/UserApplication.java
@@ -3,9 +3,11 @@ package com.flash.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableFeignClients
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.flash.base", "com.flash.user"})
 public class UserApplication {
 
     public static void main(String[] args) {
