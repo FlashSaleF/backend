3fd4ba4b4a3bcf487f806246676d75f66a1476a3
dnjawm19 <dnjawm19@naver.com>
2024-10-01 13:07:15 +0900

    refactor: dto로 변환시켜주는 mapper 클래스 생성

--- com.flash.flashsale/src/main/java/com/flash/flashsale/application/service/FlashSaleService.java
+++ com.flash.flashsale/src/main/java/com/flash/flashsale/application/service/FlashSaleService.java
@@ -1,5 +1,6 @@
 package com.flash.flashsale.application.service;
 
+import com.flash.flashsale.application.dto.mapper.FlashSaleMapper;
 import com.flash.flashsale.application.dto.request.FlashSaleRequestDto;
 import com.flash.flashsale.application.dto.response.FlashSaleResponseDto;
 import com.flash.flashsale.domain.model.FlashSale;
@@ -11,6 +12,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class FlashSaleService {
+
+    private final FlashSaleMapper flashSaleMapper;
     private final FlashSaleRepository flashSaleRepository;
 
     @Transactional
@@ -19,7 +22,7 @@ public class FlashSaleService {
         validDuplicateDate(flashSaleRequestDto);
 
         FlashSale flashSale = flashSaleRepository.save(FlashSale.create(flashSaleRequestDto));
-        return convertResponseDto(flashSale);
+        return flashSaleMapper.convertToResponseDto(flashSale);
     }
 
     private void validDuplicateDate(FlashSaleRequestDto flashSaleRequestDto) {
@@ -33,14 +36,5 @@ public class FlashSaleService {
         if (flashSaleRequestDto.endDate().isBefore(flashSaleRequestDto.startDate())) {
             throw new IllegalArgumentException("종료일은 시작일보다 빠를 수 없습니다.");
         }
-    }
-
-    private FlashSaleResponseDto convertResponseDto(FlashSale flashSale) {
-        return new FlashSaleResponseDto(
-            flashSale.getId(),
-            flashSale.getName(),
-            flashSale.getStartDate(),
-            flashSale.getEndDate()
-        );
     }
 }
