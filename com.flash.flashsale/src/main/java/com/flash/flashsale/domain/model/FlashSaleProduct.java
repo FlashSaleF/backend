9c2e9f0d16ea84b3b1b3520be4937db3791cbd10
dnjawm19 <dnjawm19@naver.com>
2024-10-01 17:30:51 +0900

    feat: 플래시 세일 상품 entity 생성

---
+++ com.flash.flashsale/src/main/java/com/flash/flashsale/domain/model/FlashSaleProduct.java
@@ -0,0 +1,37 @@
+package com.flash.flashsale.domain.model;
+
+import com.flash.base.jpa.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "p_flash_sale_products")
+public class FlashSaleProduct extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "flash_sale_id")
+    private FlashSale flashSale;
+
+    @Column(nullable = false)
+    private UUID productId;
+    @Column(nullable = false)
+    private Integer salePrice;
+    @Column(nullable = false)
+    private Integer stock;
+    @Enumerated(EnumType.STRING)
+    private FlashSaleProductStatus status;
+    @Column(nullable = false)
+    private LocalDateTime startTime;
+    @Column(nullable = false)
+    private LocalDateTime endTime;
+}
---
+++ com.flash.flashsale/src/main/java/com/flash/flashsale/domain/repository/FlashSaleProductRepository.java
@@ -0,0 +1,4 @@
+package com.flash.flashsale.domain.repository;
+
+public interface FlashSaleProductRepository {
+}
