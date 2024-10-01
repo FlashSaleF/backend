14d4c27a508e1143207da40066b5bd58347c8cca
krystal-13 <honey9163@gmail.com>
2024-10-01 10:13:00 +0900

    feat: 업체 등록 API 구현

---
+++ com.flash.vendor/src/main/java/com/flash/vendor/infrastructure/repository/JpaVendorRepository.java
@@ -0,0 +1,13 @@
+package com.flash.vendor.infrastructure.repository;
+
+import com.flash.vendor.domain.model.Vendor;
+import com.flash.vendor.domain.repository.VendorRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface JpaVendorRepository extends JpaRepository<Vendor, UUID>, VendorRepository {
+
+    Optional<Vendor> findByAddress(String address);
+}
