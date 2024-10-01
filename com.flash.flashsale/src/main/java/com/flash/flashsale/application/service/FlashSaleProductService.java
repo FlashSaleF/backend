cf489149db303c3d910f2c40da08d6ae2abee26d
dnjawm19 <dnjawm19@naver.com>
2024-10-02 01:09:54 +0900

    refactor: flashSaleService existFlashSale메소드 수정

--- com.flash.flashsale/src/main/java/com/flash/flashsale/application/service/FlashSaleProductService.java
+++ com.flash.flashsale/src/main/java/com/flash/flashsale/application/service/FlashSaleProductService.java
@@ -24,7 +24,7 @@ public class FlashSaleProductService {
         validDuplicate(flashSaleProductRequestDto);
         validAvailableDateTime(flashSaleProductRequestDto);
 
-        FlashSale flashSale = flashSaleService.excistFlashSale(flashSaleProductRequestDto.flashSaleId());
+        FlashSale flashSale = flashSaleService.existFlashSale(flashSaleProductRequestDto.flashSaleId());
 
         validAvailableSailTime(flashSale, flashSaleProductRequestDto);
 
