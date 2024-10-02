34e0538a13cc8e2f4dd4efdaf8e28e6050aaf085
macdduck <knh9612@naver.com>
2024-10-02 14:59:53 +0900

    feat: 로그인 로직

--- com.flash.auth/src/main/java/com/flash/auth/presentation/controller/AuthController.java
+++ com.flash.auth/src/main/java/com/flash/auth/presentation/controller/AuthController.java
@@ -1,15 +1,22 @@
 package com.flash.auth.presentation.controller;
 
 import com.flash.auth.application.dto.request.JoinRequestDto;
+import com.flash.auth.application.dto.request.LoginRequestDto;
 import com.flash.auth.application.dto.response.JoinResponseDto;
+import com.flash.auth.application.dto.response.LoginResponseDto;
 import com.flash.auth.application.service.AuthService;
+import com.flash.auth.application.service.util.CookieUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.flash.auth.application.service.util.JwtUtil.AUTHORIZATION_HEADER;
 
 @Slf4j(topic = "Auth Controller")
 @RestController
@@ -18,10 +25,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final CookieUtil cookieUtil;
 
     @PostMapping("/join")
-    public JoinResponseDto join(@RequestBody @Valid JoinRequestDto joinRequestDto) {
-        return authService.join(joinRequestDto);
+    public ResponseEntity<JoinResponseDto> join(@RequestBody @Valid JoinRequestDto joinRequestDto) {
+        return ResponseEntity.ok(authService.join(joinRequestDto));
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody @Valid LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        LoginResponseDto loginResponseDto = authService.attemptAuthentication(loginRequestDto);
+        response.addHeader(AUTHORIZATION_HEADER, loginResponseDto.accessToken());
+        response.addCookie(cookieUtil.createCookieWithRefreshToken(loginResponseDto.refreshToken()));
+
+        return ResponseEntity.ok(loginResponseDto.id());
+    }
 }
