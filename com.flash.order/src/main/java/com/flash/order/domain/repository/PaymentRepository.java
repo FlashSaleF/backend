8400c4bcc6cab634d0e401298c9342d7b9f80287
lsw71311 <lsw71311@naver.com>
2024-10-06 17:14:22 +0900

    feat: paymentUid로 결제 조회

--- com.flash.order/src/main/java/com/flash/order/domain/repository/PaymentRepository.java
+++ com.flash.order/src/main/java/com/flash/order/domain/repository/PaymentRepository.java
@@ -2,8 +2,12 @@ package com.flash.order.domain.repository;
 
 import com.flash.order.domain.model.Payment;
 
+import java.util.Optional;
+
 public interface PaymentRepository {
     void delete(Payment payment);
 
     Payment save(Payment payment);
+
+    Optional<Payment> findByPaymentUid(String paymentUid);
 }
