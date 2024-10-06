0ab30747793ff43837a52de7865cfafed6296364
krystal-13 <honey9163@gmail.com>
2024-10-06 18:46:20 +0900

    chore: .yml 변경

--- com.flash.vendor/src/main/java/com/flash/vendor/infrastructure/client/UserFeignClient.java
+++ com.flash.vendor/src/main/java/com/flash/vendor/infrastructure/client/UserFeignClient.java
@@ -5,8 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-// TODO route id or url
-@FeignClient(name = "user", url = "${client.url.user}")
+@FeignClient(name = "user")
 public interface UserFeignClient {
 
     @GetMapping("/api/internal/users/{userId}")
