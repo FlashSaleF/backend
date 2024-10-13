a4ecdf6561e193c4e6ab90f4d8c9a9db548de734
dnjawm19 <dnjawm19@naver.com>
2024-10-14 03:40:30 +0900

    feat: openFeign Patch 매서드 사용을 위한 설정 추가

--- com.flash.flashsale/src/main/java/com/flash/flashsale/infrastructure/configuration/SwaggerConfig.java
+++ com.flash.flashsale/src/main/java/com/flash/flashsale/infrastructure/configuration/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.flash.flashsale.infrastructure.client.configuration;
+package com.flash.flashsale.infrastructure.configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
