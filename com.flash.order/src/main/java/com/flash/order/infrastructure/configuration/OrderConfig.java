890be4e5713ae4dfd6c40c6fc998ae314f0d15ea
lsw71311 <lsw71311@naver.com>
2024-10-14 03:10:07 +0900

    feat: redissonclient 빈 등록

--- com.flash.order/src/main/java/com/flash/order/infrastructure/configuration/OrderConfig.java
+++ com.flash.order/src/main/java/com/flash/order/infrastructure/configuration/OrderConfig.java
@@ -1,6 +1,8 @@
 package com.flash.order.infrastructure.configuration;
 
 import com.siot.IamportRestClient.IamportClient;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,5 +18,10 @@ public class OrderConfig {
     @Bean
     public IamportClient iamportClient() {
         return new IamportClient(apiKey, apiSecret);
+    }
+
+    @Bean
+    public RedissonClient redissonClient() {
+        return Redisson.create();
     }
 }
