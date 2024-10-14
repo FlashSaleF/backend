5576a28cc4f78bcf1e36ea253628eedd74e5a983
lsw71311 <lsw71311@naver.com>
2024-10-14 16:30:45 +0900

    feat: RedissonConfig 설정 추가

--- com.flash.order/src/main/java/com/flash/order/infrastructure/configuration/OrderConfig.java
+++ com.flash.order/src/main/java/com/flash/order/infrastructure/configuration/OrderConfig.java
@@ -3,6 +3,7 @@ package com.flash.order.infrastructure.configuration;
 import com.siot.IamportRestClient.IamportClient;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,10 +11,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OrderConfig {
     @Value("${iamport.apiKey}")
-    String apiKey;
+    private String apiKey;
 
     @Value("${iamport.apiSecret}")
-    String apiSecret;
+    private String apiSecret;
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
 
     @Bean
     public IamportClient iamportClient() {
@@ -22,6 +29,10 @@ public class OrderConfig {
 
     @Bean
     public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://" + redisHost + ":" + redisPort);
+
         return Redisson.create();
     }
 }
