15d067e1683f4a4816ca1dfe8d2e8b797b0f2c14
macdduck <knh9612@naver.com>
2024-10-15 23:32:08 +0900

    feat: base모듈의 예외처리를 사용하기 위한 설정

--- com.flash.auth/src/main/java/com/flash/auth/AuthApplication.java
+++ com.flash.auth/src/main/java/com/flash/auth/AuthApplication.java
@@ -3,9 +3,11 @@ package com.flash.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableFeignClients
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.flash.base.exception", "com.flash.base.jpa", "com.flash.auth"})
 public class AuthApplication {
 
     public static void main(String[] args) {
