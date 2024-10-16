b5de31a8bb4364d3f3c0dc8886fa663da5e5f5c9
Krystal-13 <honey9163@gmail.com>
2024-10-16 10:02:10 +0900

    refactor: 업체 등록할 때 customer 권한 제어

--- com.flash.order/src/main/java/com/flash/order/infrastructure/client/FlashSaleProductFeignClient.java
+++ com.flash.order/src/main/java/com/flash/order/infrastructure/client/FlashSaleProductFeignClient.java
@@ -1,8 +1,8 @@
 package com.flash.order.infrastructure.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.UUID;
 
--- com.flash.order/src/main/java/com/flash/order/infrastructure/client/ProductFeignClient.java
+++ com.flash.order/src/main/java/com/flash/order/infrastructure/client/ProductFeignClient.java
@@ -5,8 +5,8 @@ import com.flash.order.application.dtos.response.ProductResponseDto;
 import com.flash.order.application.dtos.response.ProductStockDecreaseResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.UUID;
