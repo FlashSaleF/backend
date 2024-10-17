8b6f705cc3d8724dd7793101d4778c12261efa0c
Krystal-13 <honey9163@gmail.com>
2024-10-17 11:51:35 +0900

    refactor: 재고 부족 에러 코드 507 로 변경

--- com.flash.vendor/src/main/java/com/flash/vendor/domain/exception/ProductErrorCode.java
+++ com.flash.vendor/src/main/java/com/flash/vendor/domain/exception/ProductErrorCode.java
@@ -7,7 +7,7 @@ public enum ProductErrorCode implements ErrorCode {
 
     CANNOT_MODIFY_PRODUCT(HttpStatus.FORBIDDEN, "해당 업체의 상품만 수정할 수 있습니다."),
     INVALID_PERMISSION_REQUEST(HttpStatus.BAD_REQUEST, "유효하지 않은 권한 요청입니다."),
-    INSUFFICIENT_STOCK(HttpStatus.CONFLICT, "재고가 부족합니다."),
+    INSUFFICIENT_STOCK(HttpStatus.INSUFFICIENT_STORAGE, "재고가 부족합니다."),
     INVALID_QUANTITY(HttpStatus.BAD_REQUEST, "수량은 0보다 커야 합니다."),
     UNKNOWN_PRODUCT_STATUS(HttpStatus.BAD_REQUEST, "알 수 없는 상품 상태입니다."),
     PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 ID로 등록된 상품이 없습니다.");
