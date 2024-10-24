ff4a14a93a52a98b0dc6312dc7404d3b9b5f7ac3
dnjawm19 <dnjawm19@naver.com>
2024-10-24 15:41:15 +0900

    fix: 레디스 설정 변경

--- com.flash.flashsale/src/main/java/com/flash/flashsale/infrastructure/configuration/RedisConfig.java
+++ com.flash.flashsale/src/main/java/com/flash/flashsale/infrastructure/configuration/RedisConfig.java
@@ -3,6 +3,7 @@ package com.flash.flashsale.infrastructure.configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.flash.flashsale.application.dto.response.FlashSaleProductListResponseDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -13,9 +14,14 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
     @Bean
